efeb4c6352610d68f95b2be6649f992ec1e53e12
5yndr0m <sdilanjana21@gmail.com>
2025-01-04 12:09:56 +0530

    feat: adding paper components

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,52 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import UOV_Banner from '../components/UOV_Banner';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, TextInput, Button } from "react-native-paper";
+import UOV_Banner from "../components/UOV_Banner";
 
 export default function LoginPage() {
+  const [text, setText] = useState("");
+
   return (
-      <ScrollView>
-        
-        <View style={styles.container}>
+    <ScrollView>
+      <View style={styles.container}>
         <View style={styles.header}>
           <UOV_Banner />
         </View>
-        <View style={styles.banner}>
+        <View style={styles.title}>
+          <Text variant="displaySmall" style={styles.titleText}>
+            STUDENT LOGIN
+          </Text>
+        </View>
+        <View style={styles.container2}>
+          <TextInput
+            mode="outlined"
+            label="Username"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+          <TextInput
+            mode="outlined"
+            label="Password"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+          <Button mode="contained" onPress={() => console.log("Pressed")}>
+            Login
+          </Button>
+        </View>
+        <View style={styles.footer}>
           <Text>Open up App.js to start working on your app!</Text>
         </View>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-        </View>
-      </ScrollView>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   header: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: "#fff",
   },
-  banner: {
-    flex: 1,
-    backgroundColor: 'blue',
+  title: {
+    flex: 2,
+    backgroundColor: "#fff",
+  },
+  titleText: {
+    textAlign: "center",
+    marginTop: 20,
+    marginBottom: 20,
   },
   container2: {
     flex: 3,
-    backgroundColor: 'red',
+    backgroundColor: "red",
   },
   footer: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: "#e2bee2",
   },
-  safe: {
-    flex: 1,
-  }
 });
