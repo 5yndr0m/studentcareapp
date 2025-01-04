758e49374f3ad5ea7b5f0820a92df69a67e2ebaf
5yndr0m <sdilanjana21@gmail.com>
2025-01-04 15:14:53 +0530

    feat: implement and populate bottom tab

--- App.js
+++ App.js
@@ -2,13 +2,64 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import LoginPage from "./pages/LoginPage";
 import Profile from "./pages/Profile";
 import Courses from "./pages/Courses";
 import Subjects from "./pages/Subjects";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#e2bee2",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: { backgroundColor: "white" },
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Courses"
+        component={Courses}
+        options={{
+          tabBarLabel: "Cources",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="school" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Subjects"
+        component={Subjects}
+        options={{
+          tabBarLabel: "Subjects",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="book-open"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 export default function App() {
   return (
     <SafeAreaProvider>
@@ -33,12 +84,13 @@ export default function App() {
             options={{ title: "Uov Student care" }}
           />
           <Stack.Screen
-            name="Profile"
-            component={Profile}
-            options={{ headerBackVisible: false, title: "Uov Student care" }}
+            name="MainTabs"
+            component={MainTabs}
+            options={{
+              headerBackVisible: false,
+              title: "Uov Student care",
+            }}
           />
-          <Stack.Screen name="Courses" component={Courses} />
-          <Stack.Screen name="Subjects" component={Subjects} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
