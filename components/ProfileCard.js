11428a1a4971926ba0ca8f682d1daaf9906e0403
5yndr0m <sdilanjana21@gmail.com>
2025-01-05 07:07:46 +0530

    feat: creating profile card skeloton

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,21 +1,29 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import { Card, Avatar } from 'react-native-paper';
+import { View, Text, TouchableOpacity } from "react-native";
+import { Card, Avatar, Divider } from "react-native-paper";
 
 const ProfileCard = ({ student }) => {
   return (
     <Card style={{ margin: 10 }}>
       <Card.Content>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Avatar.Image
-            size={100}
-            source={student?.profile_pic}
-            style={{ marginRight: 10 }}
-          />
+        <View style={{ flexDirection: "column", alignItems: "center" }}>
+          <Avatar.Image size={200} source={student?.profile_pic} />
           <View>
-            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
               {student?.name}
             </Text>
-            <Text style={{ fontSize: 16 }}>{student?.email}</Text>
+            <Text style={{ fontSize: 16 }}>
+              Age: {student?.age} | Gender: {student?.gender}
+            </Text>
+            <Divider />
+            <Text>Contact Information</Text>
+            <Text>Email: {student?.email}</Text>
+            <Text>Phone: {student?.phone}</Text>
+            <Text>Address: {student?.address}</Text>
+            <Divider />
+            <Text>Biological Information</Text>
+            <Text>Gender: {student?.gender}</Text>
+            <Text>Age: {student?.age}</Text>
+            <Text>Blood Group: {student?.blood_group}</Text>
           </View>
         </View>
       </Card.Content>
@@ -23,4 +31,4 @@ const ProfileCard = ({ student }) => {
   );
 };
 
-export default ProfileCard;         
+export default ProfileCard;
