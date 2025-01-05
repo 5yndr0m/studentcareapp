14cb3dbef2a08049bbde4b41a79ecde021376fd4
5yndr0m <sdilanjana21@gmail.com>
2025-01-05 07:43:08 +0530

    style: adjusting card styles

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,34 +1,88 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import { Card, Avatar, Divider } from "react-native-paper";
+import { View, TouchableOpacity } from "react-native";
+import { Card, Avatar, Divider, Text } from "react-native-paper";
+import { StyleSheet } from "react-native";
 
 const ProfileCard = ({ student }) => {
   return (
-    <Card style={{ margin: 10 }}>
+    <Card>
       <Card.Content>
-        <View style={{ flexDirection: "column", alignItems: "center" }}>
-          <Avatar.Image size={200} source={student?.profile_pic} />
-          <View>
-            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+        <View style={styles.container}>
+          <View sttle={styles.overview}>
+            <Avatar.Image
+              size={200}
+              source={student?.profile_pic}
+              style={styles.avatar}
+            />
+
+            <Text variant="headlineSmall" style={styles.name}>
               {student?.name}
             </Text>
-            <Text style={{ fontSize: 16 }}>
+            <Text style={styles.basicInfo}>
               Age: {student?.age} | Gender: {student?.gender}
             </Text>
-            <Divider />
-            <Text>Contact Information</Text>
-            <Text>Email: {student?.email}</Text>
-            <Text>Phone: {student?.phone}</Text>
-            <Text>Address: {student?.address}</Text>
-            <Divider />
-            <Text>Biological Information</Text>
-            <Text>Gender: {student?.gender}</Text>
-            <Text>Age: {student?.age}</Text>
-            <Text>Blood Group: {student?.blood_group}</Text>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.section}>
+            <Text variant="titleMedium" style={styles.sectionTitle}>
+              Contact Information
+            </Text>
+            <Text style={styles.infoText}>Email: {student?.email}</Text>
+            <Text style={styles.infoText}>Phone: {student?.phone}</Text>
+            <Text style={styles.infoText}>Address: {student?.address}</Text>
+          </View>
+          <Divider style={styles.divider} />
+          <View style={styles.section}>
+            <Text variant="titleMedium" style={styles.sectionTitle}>
+              Biological Information
+            </Text>
+            <Text style={styles.infoText}>Gender: {student?.gender}</Text>
+            <Text style={styles.infoText}>Age: {student?.age}</Text>
+            <Text style={styles.infoText}>
+              Blood Group: {student?.blood_group}
+            </Text>
           </View>
         </View>
       </Card.Content>
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+  },
+  overview: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 20,
+  },
+  avatar: {
+    marginBottom: 15,
+  },
+  name: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  basicInfo: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 10,
+  },
+  divider: {
+    marginVertical: 10,
+    height: 1,
+  },
+  section: {
+    marginVertical: 5,
+  },
+  sectionTitle: {
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: "#333",
+  },
+  infoText: {
+    fontSize: 16,
+  },
+});
 
 export default ProfileCard;
